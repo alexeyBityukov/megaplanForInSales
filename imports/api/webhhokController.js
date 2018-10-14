5171e946f26cf608fadc0dcce46fc7ad987c4d75
Alexey Bityukov <alex.bityuckov@gmail.com>
2018-10-14 21:28:29 +0300

    Fixed bug with height load orders from unpaid user

--- imports/api/webhhokController.js
+++ imports/api/webhhokController.js
@@ -5,8 +5,10 @@ import Megaplan from './sendOrder';
 
 Meteor.methods({
     webhookController(order) {
-        const applicationIsCharge = inSalesId => {
-            const shop = Shops.findOne({inSalesId});
+        const inSalesId = order.account_id.toString();
+        const shop = Shops.findOne({inSalesId});
+
+        const applicationIsCharge = () => {
             if(!shop.lockDate)
                 return false;
             const lockDate = new Date(shop.lockDate);
@@ -15,28 +17,41 @@ Meteor.methods({
             return 0 < lockDate.valueOf() - now.valueOf();
         };
 
-        const inSalesId = order.account_id.toString();
-        if(!applicationIsCharge(inSalesId)) {
-            try {
-                const response = inSalesApiGet(order.account_id.toString(), 'recurring_application_charge.json');
-                if (response && 'data' in response && response.data.monthly !== null) {
-                    Shops.upsert(
-                        { inSalesId },
-                        { $set: { lockDate: response.data.paid_till}}
-                    );
-                    if(applicationIsCharge(inSalesId)) {
-                        const megaplan = new Megaplan(order);
-                        megaplan.sendOrder();
-                        return true;
+        const shopIsBanned = () => {
+            return 'shopBanned' in shop && shop.shopBanned === true;
+        };
+        if(!applicationIsCharge()) {
+            if(!shopIsBanned())
+            {
+                try {
+                    const response = inSalesApiGet(order.account_id.toString(), 'recurring_application_charge.json');
+                    if (response && 'data' in response && response.data.monthly !== null) {
+                        Shops.upsert(
+                            {inSalesId},
+                            {$set: {
+                                lockDate: response.data.paid_tillm,
+                                shopBanned: false
+                            }}
+                        );
+                        if (applicationIsCharge(inSalesId)) {
+                            const megaplan = new Megaplan(order);
+                            megaplan.sendOrder();
+                            return true;
+                        }
+                        else {
+                            Shops.upsert(
+                                {inSalesId},
+                                {$set: {shopBanned: true}}
+                            );
+                            return false;
+                        }
                     }
                     else
                         return false;
                 }
-                else
+                catch (e) {
                     return false;
-            }
-            catch (e) {
-                return false;
+                }
             }
         }
         else {
